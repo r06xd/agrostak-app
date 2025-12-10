a3b93079c6188b7fd21bfa73fcf20b25a0a07e6f
Rogger Lindao <r06xd@hotmail.es>
2025-12-09 21:41:50 -0500

    Correccion para listar recursos y creacion de gitignore

--- backend/app/resources/domain/schemas.py
+++ backend/app/resources/domain/schemas.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional
 from .entities import TipoRecurso, EstadoRecurso
 
@@ -31,5 +31,4 @@ class RecursoUpdate(BaseModel):
 class RecursoRead(RecursoBase):
     id_recurso: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
