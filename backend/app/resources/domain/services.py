6b7aa22021ce3dae1775e7857cc6766a74a96d8a
Rogger Lindao <r06xd@hotmail.es>
2026-04-05 11:34:11 -0500

    Correcciones para envio notificaciones recursos

--- backend/app/resources/domain/services.py
+++ backend/app/resources/domain/services.py
@@ -50,8 +50,9 @@ def asignarRecursoTarea(db: Session, id_recurso: int, id_tarea: int) -> bool:
 
 def enviarNotificacion(db: Session,estado: str, id_recurso: int):
     repoIdentity = IdentityRepository(db)
-    id_usuario = repoIdentity.get_user_admin(id_recurso);
+    id_usuario = repoIdentity.get_user_admin().id_usuario
     send_resource_status_push(id_usuario, estado, id_recurso)
+    return True
 
 def obtener_alertas_resumen(db: Session) -> RecursoAlertasResumen:
     repo = RecursoRepository(db)
